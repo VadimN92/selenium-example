50299930ef3db0ca2b2ab7069b2489aead694e8e
Vadim Nagirniak <vadim.nagirniak@lemberg.co.uk>
2018-01-25 18:00:42 +0200

    Added htpm file for testing

---
+++ selenium.test.js
@@ -0,0 +1,12 @@
+const { Builder, By, Key, until } = require('selenium-webdriver');
+
+(async function example() {
+    let driver = await new Builder().forBrowser('firefox').build();
+    try {
+        await driver.get("http://127.0.0.1:8081");
+        await driver.findElement(By.name('q'));.sendKeys('webdriver', Key.RETURN);
+        await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+    } finally {
+        await driver.quit();
+    }
+})();
